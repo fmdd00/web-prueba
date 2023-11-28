e2237cd98552295fecb61a3f74e7ca34c3cb80ba
Francisco Diaz <francisco63554@hotmail.com>
2023-11-27 19:26:52 -0500

    avance vista de detalles de concurso

--- src/components/modules/Concursos.js
+++ src/components/modules/Concursos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BarLog from "../bar/BarLog";
+import { Button, Card, CardBody } from "react-bootstrap";
 
 const Concursos = () => {
   return (
@@ -9,6 +10,37 @@ const Concursos = () => {
         <h1 className="text-left" style={{ color: "#800080" }}>
           Concursos
         </h1>
+        <div className="mt-4">
+          <div className="row g-0">
+            <div className="col-sm-6 col-md-10">
+              <h2 className="text-center">Concurso 1</h2>
+            </div>
+            <div className="col-6 col-md-2 text-center">
+              <Card className="mb-2">
+                <CardBody>Concurso 1</CardBody>
+              </Card>
+              <Card className="mb-2">
+                <CardBody>Concurso 2</CardBody>
+              </Card>
+              <Card className="mb-2">
+                <CardBody>Concurso 3</CardBody>
+              </Card>
+              <Card className="mb-2">
+                <CardBody>Concurso 4</CardBody>
+              </Card>
+              <Button
+                className="col-md-12"
+                style={{
+                  backgroundColor: "#800080",
+                  border: "none",
+                  textDecoration: "none",
+                }}
+              >
+                + Crear nuevo concurso
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
