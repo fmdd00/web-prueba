111823609592b8c5036d4d7e09fee269d2eaba2f
angel <angcuya@gmail.com>
2023-11-28 01:34:13 -0500

    solveproblem

--- src/components/modules/SolveProblems.js
+++ src/components/modules/SolveProblems.js
@@ -3,7 +3,7 @@ import BarLog from "../bar/BarLog";
 import Card from 'react-bootstrap/Card';
 import React, { useEffect, useState } from "react";
 import ProblemasList from "./ProblemasList";
-
+import { Button } from "react-bootstrap";
 
 const SolveProblems = () => {
   
@@ -28,16 +28,20 @@ const SolveProblems = () => {
         </h2>
   <div className="container mt-4 border rounded p-4">
     <p>Asegúrate de que estás iniciando tu aplicación de la manera correcta, utilizando el comando proporcionado por Create React App. Normalmente, esto es npm start. Durante la ejecución de este comando, las variables de entorno definidas en tu archivo .env deberían cargarse automáticamente.</p>
-  </div>
-
-  
-
-
-        
-
-
-       
-      </div>
+    </div>  
+  <div className="container rounded p-4 d-flex justify-content-center align-items-center">
+                <Button
+                  variant="outline-light"
+                  style={{
+                    borderColor: "#800080",
+                    textDecoration: "none",
+                    color: "#800080"
+                  }}
+                >
+                  Subir solución ➜
+                </Button>
+                </div>
+              </div>
     </div>
   );
 };
