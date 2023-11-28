9f3b02370d0c8eeaaebf4002b5fb9ac5886363c7
Francisco Diaz <francisco63554@hotmail.com>
2023-11-27 22:40:07 -0500

    avance pantalla crear desafio/problema

---
+++ src/components/modules/CrearProb.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import BarLog from '../bar/BarLog'
+
+const CrearProb = () => {
+  return (
+    <div>
+        <BarLog />
+        <div className='container mt-4'>
+
+        </div>
+    </div>
+  )
+}
+
+export default CrearProb
