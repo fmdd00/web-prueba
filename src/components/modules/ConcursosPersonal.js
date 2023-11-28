8cca59569d2b13e063b912a2fdfa8eeb40a8822e
Francisco Diaz <francisco63554@hotmail.com>
2023-11-27 23:15:06 -0500

    avance pantalla prueba/concurso personalizado

---
+++ src/components/modules/ConcursosPersonal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import BarLog from '../bar/BarLog'
+
+const ConcursosPersonal = () => {
+  return (
+    <div>
+        <BarLog />
+        <div className='container mt-4'>
+            <h1>Concursos</h1>
+        </div>
+    </div>
+  )
+}
+
+export default ConcursosPersonal
